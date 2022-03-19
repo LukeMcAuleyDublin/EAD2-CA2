36f293ab0515368545e48e23d7d581ca50d8cfcf
Luke McAuley <you@example.com>
2022-03-19 13:56:10 +0000

    Changes made to artist controller

--- service/Controllers/ArtistController.cs
+++ service/Controllers/ArtistController.cs
@@ -17,7 +17,30 @@ namespace service.Controllers
 			/*
 			 * TODO: Query the Azure SQL database and return all arists
 			 */
-			
+		}
+
+		[HttpGet("{id}")] // Return artist information on given artist ID.
+		public IEnumerable<Artist> getArtistByID()
+		{
+			/*
+			 * TODO: Query the database and return artist where ID is met.
+			 */
+		}
+
+		[HttpGet("/songs")] // Returns all songs in the database.
+		public IEnumerable<Song> getSongs()
+		{
+			/*
+			 * TODO: Query the database and return all objects of type song
+			 */
+		}
+
+		[HttpGet("songs/{id}")] // Return songs by artist of given ID.
+		public IEnumerable<Song> getSongsByArtist()
+		{
+			/* 
+			 * TODO: Query the database and return all songs by artist with given ID.
+			 */
 		}
 	}
 }
